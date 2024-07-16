7eef4b49ebab765d838f74ac5295e7193779dc41
Timz <timflex010@gmail.com>
2024-07-16 14:19:54 +0100

    Starthub

---
+++ starthub/13_06_24/index.js
@@ -0,0 +1,17 @@
+/*declaring navbar and menu icon variables*/
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("nav");
+
+/*check if menu icon is clicked or not */
+if (bar) {
+    bar.addEventListener("click", () => {
+        nav.classList.add("active");
+    })
+}
+/*click the x to close the navbar*/
+if (close) {
+    close.addEventListener("click", () => {
+        nav.classList.remove("active");
+    })
+}
