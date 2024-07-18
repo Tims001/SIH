99617f47f71578ec0f2494d3ac206f4a0ec0190a
Timz <timflex010@gmail.com>
2024-07-18 10:34:56 +0100

    Finally completed the assignment

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          damask: {
+            50: '#FDF5E7',
+            100: '#FAE9DA',
+            200: '#F4CFB4',
+            300: '#ECAF85',
+            400: '#E48453',
+            500: '#DD6531',
+            600: '#C74F27',
+            700: '#AC3B22',
+            800: '#893123',
+            900: '#6F2A17',
+            950: '#3C130E',
+          }
+        }
+      },
     },
     plugins: [],
   }
