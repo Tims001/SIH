76c8c2b8056b5da2e818b5a6e647808372aa58a3
Timz <timflex010@gmail.com>
2024-09-03 10:30:16 +0100

    .

---
+++ jsclass/new project/counter.js
@@ -0,0 +1,10 @@
+import _ from "lodash";
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(_.add(counter,1)))
+  setCounter(0)
+}
