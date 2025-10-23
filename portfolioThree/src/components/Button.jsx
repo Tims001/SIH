3db98ca68df84f6f467685365aafbdf6451dfb2b
Timz <timflex010@gmail.com>
2025-10-23 20:19:35 +0100

    About section done

--- portfolioThree/src/components/Button.jsx
+++ portfolioThree/src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({name, isBeam = flase , containerClass}) => {
     return (
-        <button className={`btn $${containerClass}`}>
+        <button className={`btn ${containerClass}`}>
             {isBeam &&(
                 <span className="relative flex h-3 w-3">
                     <span className="btn-ping"/>
