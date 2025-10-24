53d65eefccf18a79d02418a7dcf34d607a542678
Timz <timflex010@gmail.com>
2025-10-24 16:36:22 +0100

    client section done

--- portfolioThree/src/App.jsx
+++ portfolioThree/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
+import Projects from "./sections/Projects.jsx";
+import Clients from "./sections/Clients.jsx";
 
 const App = () => {
     return (
@@ -9,6 +11,8 @@ const App = () => {
             <Navbar />
             <Hero />
             <About/>
+            <Projects/>
+            <Clients/>
         </main>
     )
 }
